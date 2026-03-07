65aa3f6dd76bcd52f584f77e9750df9ae8f5c026
Andrey Rassokhin <andrey.d.rassokhin@gmail.com>
2026-03-07 14:40:53 +1100

    First pit-stop
    Created simple repeatable functions to calculate the normal CDF, d1 and d2 required to plug into BS.
    
    Implemented these functions into the BS formula for both calls and puts.

---
+++ bs_option_pricing.py
@@ -0,0 +1,33 @@
+import math
+
+
+def normal_cdf(x: float) -> float:
+    return 0.5 * (1 + math.erf(x / math.sqrt(2)))
+
+def calculate_d1(S: float, K: float, T: float, r: float, sigma: float) -> float:
+    numerator = math.log(S/K) +(r+sigma**2/2)*T 
+    denominator = sigma*T**0.5
+    return numerator/denominator
+
+def calculate_d2(S: float, K: float, T:float, sigma: float, r:float) -> float:
+    d1 = calculate_d1(S, K, T, r, sigma)
+    return d1 - sigma*T**0.5
+def black_scholes_price(
+        S:float,
+        K:float,
+        T:float,
+        r:float,
+        sigma:float,
+        option_type:str = "call"
+) -> float:
+    d1 = calculate_d1(S,K,T,r,sigma)
+    d2 = calculate_d2(S,K,T,r,sigma)
+    if option_type.lower() == "call":
+        price = S * normal_cdf(d1) - K * math.exp(-r*T) * normal_cdf(d2)
+    elif option_type.lower() == "put":
+        price = K * math.exp(-r*T) * normal_cdf(-d2) - S *normal_cdf(-d1)
+    else: 
+        raise ValueError("option_type must be 'call' or 'put'")
+    
+    return price 
+    
